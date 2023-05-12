cc2398a5582de1256e41b550ca87b4a9eb48b299
abidbid <133303127+abidbid@users.noreply.github.com>
2023-05-12 10:39:10 +0700

    Create latihan3.js

---
+++ latihan3.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+  <html>
+<head>
+  <title>belajar javacript dari nol</title>
+</head>
+<body>
+ <p>Tutorial Javascript untuk Pemula</p>
+<!-- Menyisipkan kode js eksternal -->
+<script src="kode-program.js"></script>
+</body>
+</html>
